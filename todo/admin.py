d79e4cebff40f09fc97a118a12016abfd27cc6fc
Humberto Manhani Junior <hmanhani@hotmail.com>
2026-08-18 20:19:05 +0200

    New Solution

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo.models import Tag, Task
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "content",
+        "created_at",
+        "deadline",
+        "is_done",
+    )
+    list_filter = ("is_done", "tags")
+    search_fields = ("content",)
+    filter_horizontal = ("tags",)
